837e29be126902f7ff11352afd566b701903d56d
Gavin Szczesniak <gavinszczesniak@gmail.com>
2023-01-24 14:26:47 -0500

    Adds validation if there is no more event and if a new day occurs

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 import discord
 import requests
-
+import time
 import random
 from datetime import datetime
 from dotenv import load_dotenv
@@ -16,7 +16,7 @@ intents.message_content = True
 
 
 client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!Bender ', intents=intents)
 
 @client.event
 async def on_ready():
@@ -40,25 +40,45 @@ async def on_message(message):
         response = someSentence
         await message.channel.send(response)
 
+historyListNumber = 0
+cur = time.localtime()
+
 @bot.command(name='history')
 async def hist(ctx):
+    # Getting todays date and number of times this command was used
+    global historyListNumber, cur
     month = datetime.now().month
     day = datetime.now().day
+
+    # Making the request and gathering the data
     URL = f'https://byabbe.se/on-this-day/{month}/{day}/events.json'
 
     r = requests.get(url = URL)
     data = r.json()
 
-    choosenEvent = data['events'][random.randrange(0, len(data['events']))]
+    # Checks if it is a new day, if so resets the historyListNumber to 0
+    if cur[:3] != time.localtime()[:3]:
+        historyListNumber = 0
+
+    # If there is no more history events 
+    if len(data['events']) < historyListNumber:
+        await ctx.send('Sorry, no more history of the day :(')
+        return
+
+    # Breaks the object up to use in a statement
+    # choosenEvent = data['events'][random.randrange(0, len(data['events']))]
+    choosenEvent = data['events'][historyListNumber]
     year = choosenEvent['year']
     title = choosenEvent['wikipedia'][0]['title']
     desc = choosenEvent['description']
+    historyListNumber +=1
+    print(historyListNumber)
 
+    # Creates the response
     response = (f'On this day in {year}, the {title} happened.\n'
                 f'{desc}')
 
+    # Sends the history of the day statement to Discord via bot
     await ctx.send(response)
 
-print(TOKEN)
 bot.run(TOKEN)
-
