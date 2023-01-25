3ce8d0809405c45dd9068388001ac64e38721835
Gavin Szczesniak <gavinszczesniak@gmail.com>
2023-01-25 15:49:32 -0500

    Reomoves key to API and adds the ability to use docker

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import discord
 import requests
 import time
-import random
+
 from datetime import datetime
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -10,10 +10,11 @@ from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+GEOCODE_KEY = os.getenv('GEOCODE_API_KEY')
+WEATHER_KEY = os.getenv('WEATHER_API_KEY')
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 
 client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='!Bender ', intents=intents)
@@ -72,13 +73,55 @@ async def hist(ctx):
     title = choosenEvent['wikipedia'][0]['title']
     desc = choosenEvent['description']
     historyListNumber +=1
-    print(historyListNumber)
 
     # Creates the response
     response = (f'On this day in {year}, the {title} happened.\n'
                 f'{desc}')
 
     # Sends the history of the day statement to Discord via bot
+    await ctx.send(response)
+
+
+@bot.command(name='weather')
+async def weather(ctx):
+    zipCode = ctx.message.content.split()[2]
+
+    geoloc_url = 'https://trueway-geocoding.p.rapidapi.com/Geocode'
+    geoloc_params = {'address': zipCode, 'language': 'en'}
+    geoloc_headers = {
+        'X-RapidAPI-Key': GEOCODE_KEY,
+        'X-RapidAPI-Host': 'trueway-geocoding.p.rapidapi.com'
+    }
+    geoloc_r = requests.get(url = geoloc_url, params=geoloc_params, headers=geoloc_headers)
+    geoloc_data = geoloc_r.json()
+    resultAddress = geoloc_data['results'][0]
+    longitude = resultAddress['location']['lng']
+    latitude = resultAddress['location']['lat']
+    
+    weather_url = 'https://weatherapi-com.p.rapidapi.com/forecast.json'
+    weather_params = {'q': f'{latitude},{longitude}'}
+    weather_headers = {
+        'X-RapidAPI-Key': WEATHER_KEY,
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+    }
+    weather_r = requests.get(url=weather_url, params=weather_params, headers=weather_headers)
+    weather_data = weather_r.json()
+
+    city = resultAddress['locality']
+    region = resultAddress['region']
+
+    currentTemp = weather_data['current']['temp_f']
+    condition = weather_data['current']['condition']['text']
+    windSpeed = weather_data['current']['wind_mph']
+    windDir = weather_data['current']['wind_dir']
+    vis = weather_data['current']['vis_miles']
+    
+    response = (f'The weather at {city} {region}:\n'
+    f'Current temperature is {currentTemp} fahrenheit\n'
+    f'It is a {condition} day outside\n'
+    f'Wind speed is {windSpeed} miles per hour in the direction {windDir}\n'
+    f'Visibility is {vis} miles')
+
     await ctx.send(response)
 
 bot.run(TOKEN)
